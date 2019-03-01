e10bef38204a81b112b141c1186c39a850c7cb8a
Jimmy Zebroski <zebrozkii@gmail.com>
2019-03-01 10:51:13 -0800

    Working on models currently running into a couple small errors

--- HairSalon.Solution/HairSalon/Models/Stylist.cs
+++ HairSalon.Solution/HairSalon/Models/Stylist.cs
@@ -3,10 +3,44 @@ using MySql.Data.MySqlClient;
 
 namespace HairSalon.Models
 {
-  public class Salon
+  public class Stylist
   {
   private string _name;
   private int _id;
 
+  public Stylist(string name, int id)
+  {
+    _name = name;
+    _id = id;
+  }
+  public string GetName()
+  {
+    return _name;
+  }
+  public int GetId()
+  {
+    return _id;
+  }
+  public static List<Stylist> GetAll()
+  {
+    List<Stylist> allItem = new List<Stylist>{};
+    MySqlConnection conn = DB.Connection();
+    conn.Open();
+    MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+    cmd.CommandText = @"SELECT * FROM stylist;";
+    MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+    while(rdr.Read())
+    {
+      int stylistId = rdr.GetInt32(0);
+      string stylistName = rdr.GetString(1);
+      Stylist newStylist = new Stylist(stylistName, stylistId);
+      allItems.Add(newStylist);
+    }
+    if(conn != null)
+    {
+      conn.Dispose();
+    }
+  }
+  }
 
 }
